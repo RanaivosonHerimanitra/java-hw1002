733da0ecb0d4dba8023dcf213023fa14bbfd22bf
Ranaivoson <air_manitra@yahoo.fr>
2017-01-16 22:34:07 +0300

    improve docs and style

--- Heures.java
+++ Heures.java
@@ -1,27 +1,46 @@
+//*****************************************************************************
+// Heures.java              Auteur: Herimanitra RANAIVOSON/INF1002-HW1
+//lecture du nombre de secondes et affichage en hh-mn-sec 
+//*****************************************************************************
+
 import java.util.Scanner;
-/*lecture d'un nombre de secondes et affichage H-mn-sec */
+
 public class Heures {
+	
+	//declaration des variables:
 	private static Scanner reader;
-	static long intPart1;
-	static double fracPart1;
-	static long intPart2;
-	static int fracPart2;
+	static long heures;
+	static double Nbtemp;
+	static long minutes;
+	static int secondes;
+	static String monMessage;
+	
+	
 	
 	public static void main(String[] args) {
+		//lecture input de l'user:
 		reader = new Scanner(System.in);  
 		System.out.println("Enter a number: "); 
 		int nb = reader.nextInt();
 		double nb_h= nb/3600.;
-		//separe heure et minute
-		intPart1 = (long) nb_h;
-		//récupère minute
-		fracPart1 = 60*(nb_h - intPart1);
-		//separe minute et seconde:
-		intPart2 = (long) fracPart1;
-		//recupere et arrondi la seconde
-		fracPart2 = (int) Math.round(60*(fracPart1 - intPart2));
-		//affiche resultat:
-		System.out.println(intPart1 + "heures "+ intPart2 + "minutes " + fracPart2 + "secondes");
+		
+		//separe heure(s) et minute(s):
+		heures = (long) nb_h;
+		
+		//récupère minute(s)
+		Nbtemp = 60*(nb_h - heures);
+		
+		//sépare minute(s) et seconde(s):
+		minutes = (long) Nbtemp;
+		
+		//récupère et arrondi seconde(s):
+		secondes = (int) Math.round(60*(Nbtemp - minutes));
+		
+		
+		//affichage du resultat:
+		monMessage = nb + " secondes contiennent ";
+		monMessage += heures + "heure(s), "+ minutes + "minute(s), " + secondes + "seconde(s)";
+		System.out.println(monMessage);
 	}
 	
 }
