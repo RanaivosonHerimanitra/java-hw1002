b1ad99b53ee0ad35c9a89e5cfffe0daaf7927710
Ranaivoson <air_manitra@yahoo.fr>
2017-02-02 03:10:55 +0300

    move tests into separate class

--- Ecole.java
+++ Ecole.java
@@ -78,28 +78,5 @@ public class Ecole {
 	public String getNomVille() {
 		return nomVille;
 	}
-	
-	//Test des méthodes de la classe:
-	//--------------------------------------------------------------------
-	public static void main(String[] args) throws ParseException {
-		Ecole myecole = new Ecole();
-		
-		//accesseur annee creation:
-		System.out.println("Annee de creation(accesseur): " + myecole.getAnneeCreation() );
-		
-		//voir valeur par defaut:
-		System.out.println("Nb. Eleve par defaut: " + myecole.getNbEleve()) ;
-		
-		//mutateur pour le Nb Eleve:
-		myecole.setNbEleve(15000) ;
-		
-		//voir le changement:
-		System.out.println("Nb. Eleve apres mutation: " + myecole.getNbEleve()) ;
-		
-		//test de la methode toString:
-		System.out.println(myecole.toString());
-
-		
-	}
 }
 
