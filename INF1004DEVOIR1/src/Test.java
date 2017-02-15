12865b42ce6a7ed4cff3eb8f17c85c7bb7fe605f
Ranaivoson <air_manitra@yahoo.fr>
2017-02-15 23:23:36 +0300

    update test accordingly

--- INF1004DEVOIR1/src/Test.java
+++ INF1004DEVOIR1/src/Test.java
@@ -16,9 +16,14 @@ public class Test {
 			compagnie.makeTrip();
 			compagnie.displayConfirmationMsg();
 			System.out.println("************Test de FindLimousineByChauffeur avec quelques valeurs by default************");
+			//cherche la limousine du chauffeur qui est defini par defaut:
 			compagnie.findLimousineByChauffeur("Eric", "Jean","2012");
+			//meme nom prenom qu'un chauffeur dans la base
+			//mais different annees embauches:
+			compagnie.findLimousineByChauffeur("Eric", "Jean","2011");
+			//nom prenom qui n est pas encore enregistre
 			compagnie.findLimousineByChauffeur("Xavier", "peter","2011");
-			System.out.println("****************************************************************************");
+			System.out.println("************************************************************");
 			System.out.println("************CARACTERISTIQUES DES TRAJETS EFFECTUES************");
 			compagnie.getAllTrajet();
 			
